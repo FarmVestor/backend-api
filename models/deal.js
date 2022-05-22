9062fdb7e8a27f0e03ee6d808f153b6931d15237
RashaAlmekhlafi <drrsmo@yahoo.com>
2022-05-22 06:57:34 -0700

    add deal farmkind request models

--- models/deal.js
+++ models/deal.js
@@ -11,6 +11,16 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Deal.belongsTo(models.Users, {
+        foreignKey: 'userId',
+        as: userId
+      }
+      );
+      Deal.belongsTo(models.Users, {
+        foreignKey: 'partenerId',
+        as: partenerId
+      }
+      );
     }
   }
   Deal.init({
