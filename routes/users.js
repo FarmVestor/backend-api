ede9bc21cfed262964671725e326b042150f9fcb
RashaAlmekhlafi <drrsmo@yahoo.com>
2022-05-26 12:43:14 -0700

    changes on users route

--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,18 @@ const { isAuthenticated } = require('../middlewares/isAuthenticated');
 const { isDifferentAdmin } = require('../middlewares/isDifferentAdmin');
 
 router.get('/',isAuthenticated,userController.index);
-router.get('/', isAuthenticated,userController.index);
 router.post('/', userController.signup);
 router.post('/login', userController.login);
 router.get('/:id', isAuthenticated, userController.show);
 router.put('/:id', isAuthenticated, userController.update);
 router.delete('/:id', isAuthenticated, isDifferentAdmin, userController.delete);
 
+
+// userType routes
+router.get('/userType/all',isAuthenticated,userController.indexUserType);
+router.get('/userType/:id',isAuthenticated,userController.showUserType);
+
+router.post('/userType', userController.storeUserType);
+router.put('/userType/:id', isAuthenticated, userController.updateUSerType);
+router.delete('/userType/:id', isAuthenticated, isDifferentAdmin, userController.deleteUserType);
 module.exports = router;
