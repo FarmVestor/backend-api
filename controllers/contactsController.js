5f6d0e521efeba183bc6bfca8cbf88eefdfe6c13
RashaAlmekhlafi <drrsmo@yahoo.com>
2022-06-14 07:13:17 -0700

    fix contacts route

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,63 +1,86 @@
 const nodemailer = require("nodemailer");
+const { response } = require("../app");
+
+var models = require('../models');
+
 
 
 exports.contacts = async function (req, res) {
-    // let testAccount = await nodemailer.createTestAccount();
-
-    const contactEmail = nodemailer.createTransport({
-        host: 'smtp.mail.yahoo.com',
-        port: 587,
-        secure:false,
-        auth: {
-            user: 'sara.asralan@ymail.com',
-            pass: 'tdhlknqtgeesarcy'
-        }
-    });
-    
-  
-  contactEmail.verify((error) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Ready to Send");
+    var response = {
+        success: true,
+        message: [],
+        data: {}
     }
-  });
 
 
-  const title = req.body.title;
-  const email = req.body.email;
-  const message = req.body.mssg; 
-//   const mail = {
-//     from: email,
-//     to: "amaralkhooly1@gmail.com",
-//     subject: "Contact Form Submission",
-//     html: `<p>title: ${title}</p>
-//            <p>Email: ${email}</p>
-//            <p>Message: ${message}</p>`,
-//   };
-//   await contactEmail.sendMail(mail, (error) => {
-//     if (error) {
-//         console.error(error)
-//       res.json({ status: "ERROR" });
-//     } else {
-//         console.error(error)
-//         res.json({ status: "Message Sent" });
-//     }
-//   });
-  // send mail with defined transport object
-   await contactEmail.sendMail({
-    from: email, // sender address
-    to: "ammaralkhooly1@gmail.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
-  }).then((response) => {
-  console.log("Message sent: %s", response.messageId);
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(response));
-  res.send(response)
-  }).catch((e) => {
-      console.error(e)
-      res.send(e.message)
-  })
+
+console.log("req.body.farmName",req.body.farmName)
+console.log("req.body.recieverName",req.body.recieverName)
+console.log("req.body.reciever",req.body.reciever)
+
+
+        var output = ''
+        if (req.body.farmName) {
+            output = `
+                <p> Hello ${req.body.recieverName}, 
+                a user want to reach you. he sent the following message</p>
+                <p> from : ${req.body.email} </p>
+                <p> farm : ${req.body.farmName} </p>
+                <p> message : ${req.body.mssg} </p>
+                
+                `;
+        } else {
+            output = `
+                <p> Hello ${req.body.recieverName}, 
+                a user want to reach you. he sent the following message</p>
+                <p> from : ${req.body.email} </p>
+                <p> message : ${req.body.mssg} </p>
+                
+                    `;
+        }
+
+
+        let transporter = nodemailer.createTransport({
+            host: "smtp.mail.yahoo.com",
+            service: "yahoo",
+            port: 587,
+            secure: false,
+            auth: {
+                user: "Green_Hand_1@yahoo.com",
+                pass: "wdypgwejutizpuhl",
+            },
+            debug: false,
+            logger: true
+        });
+
+        let mailOptions = {
+            from: '"GreenHand" <Green_Hand_1@yahoo.com>',
+            to: req.body.reciever,
+            subject: 'Interested',
+            text: "Request From GreenHand",
+            html: output,
+        }
+
+        transporter.sendMail(mailOptions, function (err, data) {
+            if (err) {
+                response.success = false
+                response.message.push("Email was not sent")
+                console.log("Error", err)
+                res.send(response)
+            } else {
+                response.success = true
+                response.message.push("Email sent")
+                console.log("Email sent")
+                res.send(response)
+            }
+        })
+
+    
+
+
+
+
+
+
 
 }
